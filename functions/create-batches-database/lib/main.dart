fca626bf8088dba93b780bed51d8ee7c9a601581
AmanKeswani <keswaniaman137@gmail.com>
2023-08-11 14:03:21 +0530

    removed debug print statements

--- functions/create-batches-database/lib/main.dart
+++ functions/create-batches-database/lib/main.dart
@@ -15,16 +15,12 @@ import 'package:dart_appwrite/dart_appwrite.dart';
 
 Future<void> start(final req, final res) async {
   try {
-    print({"reached": "point 1"});
     final client = Client();
-    print({"reached": "point 2"});
 
     // Uncomment the services you need, delete the ones you don't
     // final account = Account(client);
     // final avatars = Avatars(client);
     final database = Databases(client);
-    print({"reached": "point 3"});
-
     // final functions = Functions(client);
     // final health = Health(client);
     // final locale = Locale(client);
@@ -34,34 +30,25 @@ Future<void> start(final req, final res) async {
 
     if (req.variables['APPWRITE_FUNCTION_ENDPOINT'] == null ||
         req.variables['APPWRITE_FUNCTION_API_KEY'] == null) {
-      print({"reached": "point 4"});
       print(
           "Environment variables are not set. Function cannot use Appwrite SDK.");
-      print({"reached": "point 5"});
     } else {
-      print({"reached": "point 6"});
       client
           .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
           .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
           .setKey(req.variables['APPWRITE_FUNCTION_API_KEY'])
           .setSelfSigned(status: true);
-      print({"reached": "point 7"});
 
-      var abc = await database.create(
+      await database.create(
         databaseId: ID.unique(),
         name: "Batches",
       );
-      print(abc.toString());
-      print({"reached": "point 8"});
     }
 
-    print({"reached": "point 9"});
     res.json({
       'areDevelopersAwesome': true,
     });
-    print({"reached": "point 10"});
   } catch (e) {
-    print({"reached": "point 11"});
     res.json({'error': e.toString()});
   }
 }
