94da620d26d43daa12292f61cc412554e4a24918
AmanKeswani <keswaniaman137@gmail.com>
2023-08-16 09:57:33 +0530

    cloud function created and deploy to make a single database and make all collections in it and coded for students, tests, faculties collection attributes

--- functions/create-database/README.md
+++ functions/create-database/README.md
@@ -1,4 +1,4 @@
-# create-batches-database
+# create-database
 
 Welcome to the documentation of this function 👋 We strongly recommend keeping this file in sync with your function's logic to make sure anyone can easily understand your function in the future. If you don't need documentation, you can remove this file.
 
---
+++ functions/create-database/lib/main.dart
@@ -0,0 +1,243 @@
+import 'package:dart_appwrite/dart_appwrite.dart';
+import 'package:dart_appwrite/models.dart';
+
+/*
+  'req' variable has:
+    'headers' - object with request headers
+    'payload' - request body data as a string
+    'variables' - object with function variables
+
+  'res' variable has:
+    'send(text, status: status)' - function to return text response. Status code defaults to 200
+    'json(obj, status: status)' - function to return JSON response. Status code defaults to 200
+  
+  If an error is thrown, a response with code 500 will be returned.
+*/
+
+Future<void> start(final req, final res) async {
+  final client = Client();
+
+  // Uncomment the services you need, delete the ones you don't
+  // final account = Account(client);
+  // final avatars = Avatars(client);
+  final database = Databases(client);
+  // final functions = Functions(client);
+  // final health = Health(client);
+  // final locale = Locale(client);
+  // final storage = Storage(client);
+  // final teams = Teams(client);
+  // final users = Users(client);
+
+  client
+      .setEndpoint('http://192.168.251.189:80/v1')
+      .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
+      .setKey(
+          '374d5900f2399ea3c290c1128b65bdd31ff42ec01f2312af2f30da6466698bfff9ba2b2c28d1942b9ebd2c57c4664cfb46bcbda435b81370b015fc84b2019da3e943ff3958bc886edc48dfced176554eace1ee1ffb84109bc7bb6bd16e57bd2800726628fe148eb4260bc5192483d689125fa4b0c73d97e4d7163fa4825debab')
+      .setSelfSigned(status: true);
+
+  Database coreDatabase =
+      await database.create(databaseId: ID.unique(), name: "Core Database");
+
+  Collection studentCollection = await database.createCollection(
+      databaseId: coreDatabase.$id,
+      collectionId: ID.unique(),
+      name: "Students");
+  Collection facultyCollection = await database.createCollection(
+      databaseId: coreDatabase.$id,
+      collectionId: ID.unique(),
+      name: "Faculties");
+  Collection testCollection = await database.createCollection(
+      databaseId: coreDatabase.$id, collectionId: ID.unique(), name: "Tests");
+  Collection lectureCollection = await database.createCollection(
+      databaseId: coreDatabase.$id,
+      collectionId: ID.unique(),
+      name: "Lectures");
+  Collection attendanceCollection = await database.createCollection(
+      databaseId: coreDatabase.$id,
+      collectionId: ID.unique(),
+      name: "Attendance");
+  Collection marksCollection = await database.createCollection(
+      databaseId: coreDatabase.$id, collectionId: ID.unique(), name: "Marks");
+  Collection batchesCollection = await database.createCollection(
+      databaseId: coreDatabase.$id, collectionId: ID.unique(), name: "Batches");
+  Collection syllabusCollection = await database.createCollection(
+      databaseId: coreDatabase.$id,
+      collectionId: ID.unique(),
+      name: "Syllabus");
+  Collection notesCollection = await database.createCollection(
+      databaseId: coreDatabase.$id, collectionId: ID.unique(), name: "Notes");
+
+  //////////////////////////////////////////// Student Collection //////////////////////////////////////////////
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "firstName",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "lastName",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "contactNumber",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "fatherName",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "fatherContactNumber",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "motherName",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "motherContactNumber",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "batch",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "emailID",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "subjects",
+      size: 50,
+      xrequired: false,
+      array: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: studentCollection.$id,
+      key: "std",
+      size: 50,
+      xrequired: true);
+
+//////////////////////////////////////////// Faculty Collection //////////////////////////////////////////////
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: facultyCollection.$id,
+      key: "firstName",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: facultyCollection.$id,
+      key: "lastName",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: facultyCollection.$id,
+      key: "contactNumber",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: facultyCollection.$id,
+      key: "emailID",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: facultyCollection.$id,
+      key: "subjects",
+      size: 50,
+      array: true,
+      xrequired: false);
+
+//////////////////////////////////////////// Tests Collection //////////////////////////////////////////////\
+
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: testCollection.$id,
+      key: "subject",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: testCollection.$id,
+      key: "topics",
+      array: true,
+      size: 50,
+      xrequired: false);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: testCollection.$id,
+      key: "batch",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: testCollection.$id,
+      key: "std",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: testCollection.$id,
+      key: "attendanceID",
+      size: 50,
+      xrequired: true);
+  await database.createStringAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: testCollection.$id,
+      key: "marksID",
+      size: 50,
+      xrequired: true);
+  await database.createIntegerAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: testCollection.$id,
+      key: "startTime",
+      xrequired: true);
+  await database.createIntegerAttribute(
+      databaseId: coreDatabase.$id,
+      collectionId: testCollection.$id,
+      key: "endTime",
+      xrequired: true);
+
+
+
+
+
+
+      
+  // if (req.variables['APPWRITE_FUNCTION_ENDPOINT'] == null ||
+  //     req.variables['APPWRITE_FUNCTION_API_KEY'] == null) {
+  //   print(
+  //       "Environment variables are not set. Function cannot use Appwrite SDK.");
+  // } else {
+  //   client
+  //       .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
+  //       .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
+  //       .setKey(req.variables['APPWRITE_FUNCTION_API_KEY'])
+  //       .setSelfSigned(status: true);
+  // }
+
+  // res.json({
+  //   'areDevelopersAwesome': true,
+  // });
+}
